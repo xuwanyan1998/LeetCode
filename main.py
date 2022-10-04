490dade4cbed0ab0e5cc31fd296b5213c4c5f6d2
xuwanyan98 <1464668138@qq.com>
2022-10-04 15:14:03 +0800

    test

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 class Solution:
-    def longestArithSeqLength(self, nums) -> int:
-        dp = dict()
+    def canPartitionKSubsets(self, nums, k: int) -> bool:
+        if sum(nums) % k != 0: return False
+        target = sum(nums) / k
+        nums.sort(reverse=True)
+        if nums[0] > target: return False
+        bucket = [0] * k
 
-        for cur in range(len(nums)):
-            for prev in range(cur):
-                diff = nums[cur] - nums[prev]
-                dp[(cur, diff)] = dp.get((prev, diff), 1) + 1
-        for d in dp:
-            if dp[d] == max(dp.values()):
-                end, diff = d
-                ans = []
-                for i in range(end,-1,-1):
-                    if (nums[end] - nums[i]) % diff == 0:
-                        ans.append(nums[i])
-                ans.sort()
-                return ans
+        def backtrack(index, bucket):
+            if index == len(nums):
+                for i in range(k):
+                    if bucket[i] != target: return False
+                return True
 
+            for i in range(k):
+                if bucket[i] + nums[index] > target: continue
+                if i > 0:
+                    if bucket[i] == bucket[i-1]: continue
+                bucket[i] += nums[index]
+                if (backtrack(index + 1, bucket)): return True
+                bucket[i] -= nums[index]
+            return False
 
-nums = [20,1,15,3,10,5,8]
-print(Solution().longestArithSeqLength(sorted(nums)))
+        return (backtrack(0, bucket))
+print(Solution().canPartitionKSubsets(nums=[4,3,2,3,5,2,1],k=4))
